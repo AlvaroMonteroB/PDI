6aa2e5faf0b85302ebf5339d08998ba69a2ad459
AlvaroMonteroB <montero.barraza.alvaro.david@gmail.com>
2023-06-15 16:31:54 -0600

    python

---
+++ Aplicacion/image_handler.py
@@ -0,0 +1,73 @@
+import cv2 as cv
+import os
+import numpy as np
+
+
+class Image:
+    def __init__(self):
+        self.imagen=None#Matriz de la imagen
+        self.mode:int=None#Modo de lectura
+        self.byte=None#Bytes de la imagen
+        self.width=None
+        self.height=None
+    def read_img(self,name,mode:int):#Leer imagen en modo estandar, si resize=true hay que reeescalarla
+        self.imagen=cv.imread(name,mode)
+        self.height,self.width=self.imagen.shape
+        if mode==0:
+            self.mode=1
+        elif mode==1:
+            self.mode=3
+        self.byte=self.imagen.tobytes()
+
+    def read_img(self,name,mode,scale_factor):#Leer con un factor de escala 
+        self.imagen=cv.imread(name,mode)
+        height,width=self.imagen.shape
+        self.imagen=cv.resize(self.imagen,(width/scale_factor,height/scale_factor))
+        self.height,self.width=self.imagen.shape
+        if mode==0:
+            self.mode=1
+        elif mode==1:
+            self.mode=3
+        self.byte=self.imagen.tobytes()
+
+    def read_img(self,name,mode,height,width):#leer escalando a valores definidos
+        self.imagen=cv.imread(name,mode)
+        self.imagen=cv.resize(self.imagen,(width,height))
+        self.height,self.width=self.imagen.shape
+        if mode==0:
+            self.mode=1
+        elif mode==1:
+            self.mode=3
+        self.byte=self.imagen.tobytes()
+
+    def assign_img(self,input):#Asignamos una nueva imagen
+        self.imagen=input
+        self.byte=self.imagen.tobytes()
+        self.height,self.width=self.imagen.shape
+
+    def show_image(self):
+        cv.imshow("imagen",self.imagen)
+        cv.waitKey(0)
+        cv.destroyAllWindows()
+
+
+    def assign_vector(self,width,height,input,mode,img:bool):#Asignamos vector calculando la imagen
+        self.mode=mode
+        self.byte=input 
+        self.width=width
+        self.height=height
+        if img:
+            self.imagen=np.frombuffer(self.byte, dtype=np.uint8)
+            self.imagen=np.reshape(self.imagen, (self.height,self.width, 1))
+
+
+    
+
+    def canny(self):#Algoritmo de canny
+        output=Image()
+        borde=cv.Canny(self.imagen,160,600)
+        output.assign_img(borde)
+        return output
+        
+
+        
---
+++ Aplicacion/main.py
@@ -0,0 +1,111 @@
+import image_handler as ih
+import numpy as np
+
+
+image1=ih.Image()
+image2.ih.image()
+path1=input("introduce la ruta de tu imagen 1")
+path2=input("introduce la ruta de tu imagen 2")
+op=int(input("Introduce la operacion a realizar|| \n1.- Suma\n2.-Multiplicacion\n3.-Resta\n4.-And\n5.-Or\n6.-Xor\n7.-Division\n"))
+image1.read_img(path1, 0, 600 , 800)
+image2.read_img(path2, 0, 600 , 800)
+image=switch_operation[op]
+image.show_image()
+
+
+
+
+
+
+
+
+
+
+switch_operation={
+    1:suma,
+    3:Resta,
+    2:multiplicacion
+}
+
+
+def Suma(img1:ih.Image,img2:ih.Image)->ih.Image:
+    output=ih.Image()
+    mat1=img1.imagen
+    mat2=img2.imagen    
+    if img1.mode==1 and img2.mode==1:
+        mat=np.empty(img1.height,img1.width)
+        for i in range(img1.height):
+            for j in range(img1.width):
+                mat[i][j]=mat1[i][j]+mat2[i][j]
+        output.assign_img(mat)
+        return output
+    elif img1.mode==3:
+        mat=np.empty(img1.height,img1.width*3)
+        for i in range(img1.height):
+            for j in range(0,img1.width*3,3):
+                mat[i][j]=mat1[i][j]+mat2[i][j]
+                mat[i][j+1]=mat1[i][j+1]+mat2[i][j+1]
+                mat[i][j+2]=mat1[i][j+2]+mat2[i][j+2]
+        output.assign_img(mat)
+        return output
+
+def Resta(img1:ih.Image,img2:ih.Image)->ih.Image:
+    output=ih.Image()
+    mat1=img1.imagen
+    mat2=img2.imagen    
+    if img1.mode==1 and img2.mode==1:
+        mat=np.empty(img1.height,img1.width)
+        for i in range(img1.height):
+            for j in range(img1.width):
+                mat[i][j]=mat1[i][j]-mat2[i][j]
+        output.assign_img(mat)
+        return output
+    elif img1.mode==3:
+        mat=np.empty(img1.height,img1.width*3)
+        for i in range(img1.height):
+            for j in range(0,img1.width*3,3):
+                mat[i][j]=mat1[i][j]-mat2[i][j]
+                mat[i][j+1]=mat1[i][j+1]-mat2[i][j+1]
+                mat[i][j+2]=mat1[i][j+2]-mat2[i][j+2]
+        output.assign_img(mat)
+        return output
+
+def multiplicacion(img1:ih.Image,img2:ih.Image)->ih.Image:
+    output=ih.Image()
+    mat1=img1.imagen
+    mat2=img2.imagen    
+    if img1.mode==1 and img2.mode==1:
+        mat=np.empty(img1.height,img1.width)
+        for i in range(img1.height):
+            for j in range(img1.width):
+                mat[i][j]=(mat1[i][j]*mat2[i][j])/255
+        output.assign_img(mat)
+        return output
+    elif img1.mode==3:
+        mat=np.empty(img1.height,img1.width*3)
+        for i in range(img1.height):
+            for j in range(0,img1.width*3,3):
+                mat[i][j]=(mat1[i][j]*mat2[i][j])/255
+                mat[i][j+1]=(mat1[i][j+1]*mat2[i][j+1])/255
+                mat[i][j+2]=(mat1[i][j+2]*mat2[i][j+2])/255
+        output.assign_img(mat)
+        return output
+def And_(img1:ih.Image,img2:ih.Image)->ih.Image:
+    output=ih.Image()
+    mat1=img1.imagen
+    mat2=img2.imagen 
+    if img1.mode==1 and img2.mode==1:
+        mat=np.empty(img1.height,img1.width)
+        mat=np.logical_and(mat1,mat2)
+        output.assign_img(mat)
+        return output
+
+def Or_(img1:ih.Image,img2:ih.Image)->ih.Image:
+    output=ih.Image()
+    mat1=img1.imagen
+    mat2=img2.imagen 
+    if img1.mode==1 and img2.mode==1:
+        mat=np.logical_or(mat1,mat2)
+        output.assign_img(mat)
+        return output
+    
